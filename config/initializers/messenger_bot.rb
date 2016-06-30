c0f82f3f333b2ef989ca28bf098de09fd24737a9
arico93 <mizuki-ariga@dac.co.jp>
2016-06-30 09:29:00 +0000

    failed callback

--- config/initializers/messenger_bot.rb
+++ config/initializers/messenger_bot.rb
@@ -1,5 +1,5 @@
 Messenger::Bot.config do |config|
-  config.access_token = "EAAOsKn9vHH0BAAf9IIjkhBCTotvNv0NPeg2aaec781wNxNcseAhvRLYEpKJZACsEfsugSLEJLm1Jps8MtB4l1zgbX6yCWo7kZAmNgEoCuEUh7ot6EcgNsXjulZAEFDbUse0TeI7Ysk4QsJ9YKlohjQgBPjZBcVgXZCHfeXnp7DQZDZD"
+  config.access_token = "EAAOsKn9vHH0BAPWip209Rw8MiQA62mIBM4BxbKCQey0wIUsPzZBhh11MIqjZBmzQh3lorcK1sFqVlqOKa28k11xgMByshx6txsxDKlwh0Be5BUkZCjQYigzwrw9sDck6kZAS7SMf468gZCXZBcRUAxI8dZBlhtDSFxkZAZCPBhl5sOwZDZD"
   config.validation_token = "hoge"
   config.secret_token = "1b00831327f18c977a0855565ab9e7b7"
 end
