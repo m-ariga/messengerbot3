322a1cdb45abadbe52186a213f8dd63388ac6b09
arico93 <mizuki-ariga@dac.co.jp>
2016-06-30 09:12:34 +0000

    verify token

---
+++ config/initializers/messenger_bot.rb
@@ -0,0 +1,5 @@
+Messenger::Bot.config do |config|
+  config.access_token = "EAAOsKn9vHH0BAAf9IIjkhBCTotvNv0NPeg2aaec781wNxNcseAhvRLYEpKJZACsEfsugSLEJLm1Jps8MtB4l1zgbX6yCWo7kZAmNgEoCuEUh7ot6EcgNsXjulZAEFDbUse0TeI7Ysk4QsJ9YKlohjQgBPjZBcVgXZCHfeXnp7DQZDZD"
+  config.validation_token = "hoge"
+  config.secret_token = "1b00831327f18c977a0855565ab9e7b7"
+end
