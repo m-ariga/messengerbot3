6a1a9b648bed43d4447d8ccd937a020ecc95228a
arico93 <mizuki-ariga@dac.co.jp>
2016-07-01 02:39:00 +0000

    connect webhook

--- config/initializers/messenger_bot.rb
+++ config/initializers/messenger_bot.rb
@@ -1,5 +1,5 @@
 Messenger::Bot.config do |config|
-  config.access_token = "EAAOsKn9vHH0BAGtzrV0aph1B0EVbwRDzrC1OZCQFBUWM3et6Gx44uZAwsSo4xPG1T5Vl9rvlfSsLX3sVIjRJKYOyLdv48M6pCetCUuZANx6XI6Rp9BS4UZBvSpF4MPi31EhZA4t5wYt2wI96pH6IXQ6rgZBlhlBGx8qonflKXb4QZDZD"
-  config.validation_token = "hoge"
+  config.access_token = "EAAOsKn9vHH0BAFSGTyR5VTTYfsmPQxpQYaIQ5MFrzw2gAS8uIlZABPpwDAxzI8PTfiuErZAjW3Ek0N6dvt5t06aMFKpirhiv492uobOZBaPZCQThhhNwhJycJbg5rSrDWPRPr0eEBqoaOGMafAZBp0z6y7ajU3g0Plk7D1ZB05qwZDZD"
+  config.validation_token = "fuga"
   config.secret_token = "d1a51dc53bfb79284bffd8c1b0628a14"
 end
