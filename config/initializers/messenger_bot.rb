2a96f5828d412233c9910b3da8b915d47837f44b
arico93 <mizuki-ariga@dac.co.jp>
2016-07-01 02:56:04 +0000

    setup webhooks

--- config/initializers/messenger_bot.rb
+++ config/initializers/messenger_bot.rb
@@ -1,5 +1,5 @@
 Messenger::Bot.config do |config|
-  config.access_token = "EAAOsKn9vHH0BAFSGTyR5VTTYfsmPQxpQYaIQ5MFrzw2gAS8uIlZABPpwDAxzI8PTfiuErZAjW3Ek0N6dvt5t06aMFKpirhiv492uobOZBaPZCQThhhNwhJycJbg5rSrDWPRPr0eEBqoaOGMafAZBp0z6y7ajU3g0Plk7D1ZB05qwZDZD"
+  config.access_token = "EAAOsKn9vHH0BACSZBYTZBOwJadOElcDAGhXiCJeJNZAejfj2cZCpPvzFkAGKcFksO5ZBqWgSYcJytPL2WIiZAERNAfZBZCh2j6m4u8K2ZBsOSiwJonGzSJMH4Ko5JyjQwu6WTqyHVehSZBjC0dzqnfgw1K0nhLc2iRMdEySBmHLXZAebgZDZD"
   config.validation_token = "fuga"
   config.secret_token = "d1a51dc53bfb79284bffd8c1b0628a14"
 end
