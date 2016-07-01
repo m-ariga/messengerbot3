98a757041efd08d8d2c257bb4cb4f4a3a0010de3
arico93 <mizuki-ariga@dac.co.jp>
2016-07-01 02:32:38 +0000

    webhook update

--- config/initializers/messenger_bot.rb
+++ config/initializers/messenger_bot.rb
@@ -1,5 +1,5 @@
 Messenger::Bot.config do |config|
-  config.access_token = "EAAOsKn9vHH0BAAjyNUQMVV5atEiJlaC8HlkRPnNW6Di5n5lZBGCoRIFSLICdKr1O5N77AuKt5hPbaZCZB2i1bsTEbe6Ll7xZBoUfeZAKzgNLqLMZCUaTQEZBgnoHCPF3EfZBH2oe83qpX6Ai6ojxA4z15RdpprAvOOfIdVSRaWVjCwZDZD"
+  config.access_token = "EAAOsKn9vHH0BAGtzrV0aph1B0EVbwRDzrC1OZCQFBUWM3et6Gx44uZAwsSo4xPG1T5Vl9rvlfSsLX3sVIjRJKYOyLdv48M6pCetCUuZANx6XI6Rp9BS4UZBvSpF4MPi31EhZA4t5wYt2wI96pH6IXQ6rgZBlhlBGx8qonflKXb4QZDZD"
   config.validation_token = "hoge"
-  config.secret_token = "1b00831327f18c977a0855565ab9e7b7"
+  config.secret_token = "d1a51dc53bfb79284bffd8c1b0628a14"
 end
