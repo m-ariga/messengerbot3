3175c51888fbe378008e0732ad80ab6e2d455b8a
arico93 <mizuki-ariga@dac.co.jp>
2016-07-05 07:30:57 +0000

    Tue Jul  5 07:30:57 UTC 2016

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -26,8 +26,8 @@ class MessengerBotController < ActionController::Base
         # end
         
         if text == "お気に入り"
-          @favorites = @user.favorites.find_by(user_id: @user_id)
-          sender.reply({ text: "お気に入り一覧です。#{@favorites.artist}, #{@favorites.musicname}" })
+          @favorites = @user.favorites.where(user_id: @user.id)
+          sender.reply({ text: "お気に入り一覧です。#{@favorites.artist}, #{@favorites.musicname}, #{@favorites.artist}" })
         elsif text == "おすすめ"
           sender.reply({ text: "オススメの一曲はこちらです。"})
         elsif text == "こんにちは"
