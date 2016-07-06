4ebdf64d96a54949c08c9374b58703c43505a8b4
arico93 <mizuki-ariga@dac.co.jp>
2016-07-06 09:36:19 +0000

    Wed Jul  6 09:36:19 UTC 2016

--- config/initializers/messenger_bot.rb
+++ config/initializers/messenger_bot.rb
@@ -1,5 +1,5 @@
 Messenger::Bot.config do |config|
-  config.access_token = "EAAOsKn9vHH0BANcZCGPpSIbiL93pJkE5v5A6jjneIDkeY5WEIko0ngu6h20U5GcEu2tfbjbQI130J5x63FbZCSkUTqZB02lznYclJsTKEoDwvJfyyZAxdiE6CNziBvW1ZBffqzH5lqk5V3R2PFlAmbIE5ED27GfRkGArYurCnZAAZDZD"
+  config.access_token = "API_Key"
   config.validation_token = "fuga"
-  config.secret_token = "d1a51dc53bfb79284bffd8c1b0628a14"
+  config.secret_token = "secret_token"
 end
